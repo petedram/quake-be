45b36ec4b6958bf8ab16c48ff9f85c6ba5d048c3
PeteDram <me@petejames.com>
2021-04-03 22:31:16 -0700

    more ssl

--- knexfile.js
+++ knexfile.js
@@ -43,9 +43,11 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    },
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
     migrations: {
       directory: "./data/migrations",
   },
